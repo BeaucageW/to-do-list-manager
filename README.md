cea1e2b377c16ea950f6a64c57818c9969258f18
BeaucageW <williamb0235@gmail.com>
2025-11-16 13:03:34 -0500

    V2.0.0: Commands updated !
    Updated the commands to be more intuitive and added two new functions.

--- README.md
+++ README.md
@@ -1,18 +1,30 @@
 # CLI To-Do list manager
 
 ## Instructions
-Just run the program and you will be ready to start making a list. Anything you type in the terminal that isn't recognized as a command will simply be added to the list. For commands description / list, see below.
+Just run the program and you will be ready to start making a list. See the command list below for more information or run /help command in the program.
 
 ## Commands
 
-* "quit" / "exit" : Exits the program.
+* /add \<name> : Adds the value of name to the list as a task.
 
-* "print" : Displays the current list.
+* /complete \<index> : Completes the task at the specified index.
+
+* /print : Displays the current list.
+
+* /help : Displays all available commands.
+
+* /quit : Exits the program.
 
 ## Changelog
 
 **V1.0.0** [Nov15 2025] :  
 * **Added** two new commands to interact with the program : quit and print.
+
+**V2.0.0** [Nov16 2025] :  
+* **Added** two new commands : /complete and /help.
+* **Added** the ability to complete tasks with the /complete command.
+* **Changed** commands format : commands now take a backslash to make it easier to differentiate them from regular text.
+* **Removed** redundant command : exit.
 
 ## About
 
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,36 @@
-use std::io::{self, Write}; 
+use std::{io::{self, Write}}; 
 
 fn main() {
     struct ToDo{
         description: String,
-        _is_completed: bool, // will be implemented later 
+        is_completed: bool,
     }    
     
     let mut todo_list: Vec<ToDo> = Vec::new();
 
+    fn complete_command(todo: &mut ToDo){
+        todo.is_completed = true;
+    }
+    
+    fn print_command(list: &Vec<ToDo>){
+        println!("To Do List : ");
+                for todos in list{
+                    println!("- {}  [{}]", todos.description, completion_symbol(&todos));
+                }
+    }
+
+    fn add_command(description: &str, todo_list: &mut Vec<ToDo>){
+        todo_list.push(ToDo { description: (description.to_string()), is_completed: (false) });
+    }
+    
+    fn completion_symbol(todo: &ToDo) -> &str {
+        if todo.is_completed {
+            return "✓"
+        }else {
+            return "X"
+        }
+    }  
+    
     fn get_input() -> String {
         let mut input = String::new();
         
@@ -17,22 +40,46 @@ fn main() {
         
         input.trim().to_string()
     }
+    
+    fn detect_command(input: &str, todo_list: &mut Vec<ToDo>) -> bool{
+        let split_input: Vec<&str> = input.split_whitespace().collect();
 
-    'program: loop{
+        match split_input[0]{
+            "/add" => {
+                add_command(split_input[1], todo_list);
+                true
+            }
+            "/print" => {
+                print_command(todo_list);
+                true
+            }
+            "/quit" => false,
+            
+            "/complete" => {
+                complete_command(&mut todo_list[split_input[1].trim().parse::<usize>().expect("Incorrect parameter found for command 'complete'")]);
+                true
+            }
+            "/help" => {
+                println!("Available commands : 
+                    '/complete <index>' completes the task at the selected index 
+                    '/add <name>' creates a new task with the selected name 
+                    '/print' prints the current list 
+                    '/quit' exits the program ");
+            true
+            }
+            other => {
+                println!("command '{other}' not found. Type /help for list of commands");
+                true
+            }
+        }  
+    }
+
+    loop{
         print!(">");
         io::stdout().flush().unwrap();
 
-        match get_input().as_str(){
-            "print" =>{ 
-                println!("To Do List : ");
-                for todo in &todo_list{
-                    println!("- {}", todo.description);
-                }
-            }
-            
-            "quit" | "exit" => break 'program,
-            
-            other => todo_list.push(ToDo { description: (other.to_string()), _is_completed: (false) }),
-        }
+        if !detect_command(&get_input(), &mut todo_list){
+            break;
+        }      
     }
 }
